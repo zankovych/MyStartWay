c92102701fa5e487dee22a57d1a390c5575f2d4f
Nazar Zankovych <nazar.zankovych.shi.2025@lpnu.ua>
2025-09-19 00:12:19 +0300

    Refactor code using functions

--- main.cpp
+++ main.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
 #include <locale.h>
-
 #include <windows.h>
+
 using namespace std;
 
+// імплікація
+bool implication(bool a, bool b){
+    return (!a || b);
+}
+
+// Перевірка введення
+bool inputBool(string name){
+    int value;
+    do{
+        cout << "Введіть значення " << name << " : ";
+        cin >> value;
+        
+        if (value != 0 && value !=1){
+            cout << "Помилка вводу! Введіть лише 0 або 1" << endl;
+        }
+    } while (value != 0 && value != 1);
+    
+    return value;
+}
+
 int main(){
-
-
     SetConsoleOutputCP(65001); // Увімкнути UTF-8 вивід
     setlocale(LC_ALL, "uk_UA.UTF-8");
 
-    short x, y, z; //основні змінні
-    cout << "Введіть знчення x, y та z" << endl;
-    cin >> x >> y >> z;
+    bool x = inputBool("x");
+    bool y = inputBool("y");
+    bool z = inputBool("z");
+    
+    cout << "\n(x → (y → z)) → ((x ∧ y) → z)" << endl;
 
-    cout << "(x → (y → z)) → ((x ∧ y) → z)" << endl; //виведення початкової формули
+    //1. y → z
+    bool a1 = implication(y, z);
+    cout << "1. y → z : " << a1 << endl;
 
-    short a1, a2, a3, a4; //почергове виконання кожної дії
+    //2. x → (y → z)
+    bool a2 = implication(x, a1);
+    cout << "2. x → (y → z) : " << a2 << endl;
 
-    //y → z
-    if (y==0 && z==0) {a1=1; cout << "1. y → z : 1" << endl;}
-    else if (y==0 && z==1) {a1=1; cout << "1. y → z : 1" << endl;}
-        else if (y==1 && z==1) {a1=1; cout << "1. y → z : 1" << endl;}
-            else {a1=0; cout << "1. y → z : 0" << endl;}
+    //3. x ∧ y
+    bool a3 = (x && y);
+    cout << "3. x ∧ y : " << a3 << endl;
 
-    //x → (y → z)
-    if (x==0 && a1==0) {a2=1; cout << "2. x → (y → z) : 1" << endl;}
-    else if (x==0 && a1==1) {a2=1; cout << "2. x → (y → z) : 1" << endl;}
-        else if (x==1 && a1==1) {a2=1; cout << "2. x → (y → z) : 1" << endl;}
-            else {a2=0; cout << "2. x → (y → z) : 0" << endl;}
+    //4. (x ∧ y) → z
+    bool a4 = implication(a3, z);
+    cout << "4. (x ∧ y) → z : " << a4 << endl;
 
-    //x ∧ y
-    if (x==1 && y==1) {a3=1; cout << "3. x ∧ y : 1" << endl;}
-    else {a3=0; cout << "3. x ∧ y : 0" << endl;}
+    //5. (x → (y → z)) → ((x ∧ y) → z)
+    bool result = implication(a2, a4);
+    cout << "5. (x → (y → z)) → ((x ∧ y) → z) : " << result << endl;
 
-    //(x ∧ y) → z
-    if (a3==0 && z==0) {a4=1; cout << "4. (x ∧ y) → z : 1" << endl;}
-    else if (a3==0 && z==1) {a4=1; cout << "4. (x ∧ y) → z : 1" << endl;}
-        else if (a3==1 && z==1) {a4=1; cout << "4. (x ∧ y) → z : 1" << endl;}
-            else {a4=0; cout << "4. (x ∧ y) → z : 0" << endl;}
-
-    //(x → (y → z)) → ((x ∧ y) → z)
-    if (a2==0 && a4==0) cout << "5. (x → (y → z)) → ((x ∧ y) → z) : 1" << endl;
-    else if (a2==0 && a4==1) cout << "5. (x → (y → z)) → ((x ∧ y) → z) : 1" << endl;
-        else if (a2==1 && a4==1) cout << "5. (x → (y → z)) → ((x ∧ y) → z) : 1" << endl;
-            else cout << "5. (x → (y → z)) → ((x ∧ y) → z) : 0" << endl;
+    return 0;
 }
